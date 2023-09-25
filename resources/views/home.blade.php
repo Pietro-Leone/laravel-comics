2df777293a8adaca25caadfddb9ecfb21eb3fcdc
Pietro-Leone <leonepietro1999@gmail.com>
2023-09-25 19:22:25 +0200

    end milestone

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,20 @@
 @extends("layouts.public")
 
-@extends("title", "Home-Page")
+@section("title", "Home-Page")
 
 @section("content")
-    @include("partials.card")
-    @include("partials.blue-stripe")
+<div class="cards-container">
+  <div class="container position-relative d-flex justify-content-center align-items-end">
+    <div class="badge current text-bg-primary position-absolute">Current Series</div>
+    <div class="d-flex flex-wrap justify-content-center mb-5">
+      @foreach ( $comics as $element)
+      <div class="card-box">
+        @include("partials.card")
+      </div>
+      @endforeach
+    </div>
+    <div class="badge text-bg-primary position-absolute">Load more</div>
+  </div>
+</div>
+@include("partials.blue-stripe")
 @endsection
